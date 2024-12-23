4debbd39df26ecfef2960b47d1a7d829e18960ce
Azat <azatidiatov137@gmail.com>
2024-12-23 09:15:43 +0300

    улучшил HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
     <div class="home-view__content">
       <header class="home-view__header">
         <h1 class="home-view__title">
-          Saiba tudo em um só
+          <span class="home-view__title-white">Saiba tudo em um só</span>
           <span class="home-view__title-highlight">lugar.</span>
         </h1>
       </header>
@@ -29,6 +29,10 @@
 @import "@/scss/mixins.scss";
 
 .home-view {
+  width: 1920px;
+  height: 1080px;
+  background-color: black;
+
   &__logo {
     > img {
       width: 397px;
@@ -46,45 +50,57 @@
     top: 399px;
     left: 210px;
 
-    &-header {
+    &__header {
       margin-bottom: 16px;
 
-      &-title {
+      &__title {
         font-family: Inter, serif;
         font-size: 72px;
         font-weight: 700;
         line-height: 87.14px;
+      }
 
-        &-highlight {
-          color: #44c765;
-        }
+      &__title-white {
+        color: #ffffff;
+      }
+
+      &__title-highlight {
+        color: #0000ff;
       }
     }
 
-    &-subtitle {
-      @include text-inter;
-      background-color: #44c765;
+    &__subtitle {
       padding: 8px;
+      font-family: Inter, sans-serif;
+      font-size: 18px;
+      line-height: 22px;
+      font-weight: 400;
+      color: #44c765;
     }
   }
 
   &__details {
     width: 397px;
-    margin-top: 16px;
+    height: 84px;
+    top: 884px;
+    left: 210px;
+    position: absolute;
 
     &-text {
       @include text-inter;
-      background-color: #ffffff;
+      color: #ffffff;
       padding: 8px;
     }
   }
 
   &__images {
-    margin-top: 32px;
+    left: 897px;
+    position: absolute;
 
     > img {
       width: 773px;
       height: 1080px;
+      opacity: 80%;
     }
   }
 }
